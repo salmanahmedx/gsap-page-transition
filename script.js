648c355067f2a3a2c8cc74d9c7f6071d4f65b1f7
Salman Ahmed <salmanahmedx@hotmail.com>
2022-05-13 13:30:14 +0600

    page enter transition functionality

--- script.js
+++ script.js
@@ -20,8 +20,22 @@ const leaveAnimation = (current, done) => {
     tlLeave.fromTo(arrow, { opacity: 1, y: 0 }, { opacity: 0, y: 50 });
     tlLeave.fromTo(product, { y: 0, opacity: 1 }, { y: 100, opacity: 0, onComplete: done }, "<");
     tlLeave.fromTo(text, { y: 0, opacity: 1 }, { opacity: 0, y: 100 }, "<")
+    tlLeave.fromTo(circles, { y: 0, opacity: 1 }, { y: -200, opacity: 0, stagger: 0.15, ease: "back.out(1.7)", duration: 1 }, "<")
+};
 
-}
+const enterAnimation = (current, done) => {
+    const product = current.querySelector('.image-container');
+    let text = current.querySelector(".showcase-text");
+    const circles = current.querySelectorAll(".circle");
+    const arrow = current.querySelector(".showcase-arrow");
+
+    tlLeave.fromTo(arrow, { opacity: 0, y: 50 }, { opacity: 1, y: 0 });
+    tlLeave.fromTo(product, { y: -100, opacity: 0 }, { y: 0, opacity: 1, onComplete: done }, "<");
+    tlLeave.fromTo(text, { y: 100, opacity: 0 }, { opacity: 1, y: 0 }, "<")
+    tlLeave.fromTo(circles, { y: -200, opacity: 0 }, { y: 0, opacity: 1, stagger: 0.15, ease: "back.out(1.7)", duration: 1 }, "<")
+};
+
+
 
 //animations
 barba.init({
@@ -40,7 +54,7 @@ barba.init({
             enter(data) {
                 const done = this.async();
                 let next = data.next.container;
-                gsap.fromTo(next, { opacity: 0 }, { opacity: 1, duration: 3, onComplete: done })
+                enterAnimation(next, done)
             }
         }
     ]
